5157299e195aea04b12d3b2834572886a7572cc6
Michał Król <mharnen@gmail.com>
2018-05-30 16:19:47 +0100

    svg -> png

--- img/convert.sh
+++ img/convert.sh
@@ -3,6 +3,6 @@
 for file in *.svg
 do
     filename=`basename $file svg`
-    convert -background none -density 500 $file ${filename}png
+    convert -background none -density 1000 $file ${filename}png
 done
     
